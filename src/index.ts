44fd28df7bd198772d48343e97bc4da5bfcf1a78
maciejbilinski <maciej.bilinski.programming@gmail.com>
2023-01-10 00:28:24 +0100

    Example of querying with OracleDB without TypeORM

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,49 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-import "reflect-metadata";
-import * as oracledb from 'oracledb';
+import {initOracleClient, createPool} from 'oracledb';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-import { DataSource } from "typeorm"
+initOracleClient({libDir: process.env.ORACLE_CLIENT_PATH});
 
-const AppDataSource = new DataSource({
-    type: "oracle",
-    connectString: "admlab2.cs.put.poznan.pl:1521/dblab02_students.cs.put.poznan.pl",
-    username: process.env.DB_USER,
-    password: process.env.DB_USER,
-    synchronize: true
-});
+async function run() {
+  let pool, result;
 
-AppDataSource.initialize().then(() => {
+  try {
+    pool = await createPool({
+      user          : process.env.DB_USER,
+      password      : process.env.DB_USER,
+      connectString : "admlab2.cs.put.poznan.pl:1521/dblab02_students.cs.put.poznan.pl",
+    });
+
+    let connection;
+    try {
+      connection = await pool.getConnection();
+      result = await connection.execute(`SELECT table_name FROM user_tables`);
+      console.log("Result is:", result);
+    } catch (err) {
+      throw (err);
+    } finally {
+      if (connection) {
+        try {
+          await connection.close(); // Put the connection back in the pool
+        } catch (err) {
+          throw (err);
+        }
+      }
+    }
+  } catch (err: any) {
+    console.error(err.message);
+  } finally {
+    await pool?.close();
+  }
+}
+
+
+run().then(() => {
     console.log("Data Source has been initialized!");
 
     app.get('/', (req: Request, res: Response) => {
