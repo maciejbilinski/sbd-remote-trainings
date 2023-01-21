c24beeb314cd37113bf749d390390569b8e71c8e
maciejbilinski <maciej.bilinski.programming@gmail.com>
2023-01-21 21:56:13 +0100

    Training plan creator backend & frontend fixes

--- scripts/plan_creator.js
+++ scripts/plan_creator.js
@@ -50,73 +50,89 @@ window.addEventListener('load', function(){
     })
     form.addEventListener('submit', function(e){
         e.preventDefault();
+        var items = Array.from(container.querySelectorAll('.list-item'));
+        if(items.every((value) => {
+            var valueDow = value.querySelector('input[name$="[dow]"]');
+            var valueHour = value.querySelector('input[name$="[hour]"]');
+            if(valueDow.value === dow.value && valueHour.value === hour.value){
+                return false;
+            }
+            return true;
+        })){
 
-        const listItem = document.createElement('div');
-        listItem.classList.add('list-item');
-        listItem.setAttribute('item-id', nextIndex);
-
-        const dowInput = document.createElement('input');
-        dowInput.setAttribute('type', 'hidden');
-        dowInput.setAttribute('name', `days[${nextIndex}][dow]`);
-        dowInput.value = dow.value;
-        listItem.appendChild(dowInput);
-
-        const hourInput = document.createElement('input');
-        hourInput.setAttribute('type', 'hidden');
-        hourInput.setAttribute('name', `days[${nextIndex}][hour]`);
-        hourInput.value = hour.value;
-        listItem.appendChild(hourInput)
-
-        if(check.checked){
-            const ndowInput = document.createElement('input');
-            ndowInput.setAttribute('type', 'hidden');
-            ndowInput.setAttribute('name', `days[${nextIndex}][ndow]`);
-            ndowInput.value = ndow.value;
-            listItem.appendChild(ndowInput);
-
-            const nhourInput = document.createElement('input');
-            nhourInput.setAttribute('type', 'hidden');
-            nhourInput.setAttribute('name', `days[${nextIndex}][nhour]`);
-            nhourInput.value = nhour.value;
-            listItem.appendChild(nhourInput)
+            const listItem = document.createElement('div');
+            listItem.classList.add('list-item');
+            listItem.setAttribute('item-id', nextIndex);
+    
+            const dowInput = document.createElement('input');
+            dowInput.setAttribute('type', 'hidden');
+            dowInput.setAttribute('name', `days[${nextIndex}][dow]`);
+            dowInput.value = dow.value;
+            listItem.appendChild(dowInput);
+    
+            const hourInput = document.createElement('input');
+            hourInput.setAttribute('type', 'hidden');
+            hourInput.setAttribute('name', `days[${nextIndex}][hour]`);
+            hourInput.value = hour.value;
+            listItem.appendChild(hourInput)
+    
+            if(check.checked){
+                const ndowInput = document.createElement('input');
+                ndowInput.setAttribute('type', 'hidden');
+                ndowInput.setAttribute('name', `days[${nextIndex}][ndow]`);
+                ndowInput.value = ndow.value;
+                listItem.appendChild(ndowInput);
+    
+                const nhourInput = document.createElement('input');
+                nhourInput.setAttribute('type', 'hidden');
+                nhourInput.setAttribute('name', `days[${nextIndex}][nhour]`);
+                nhourInput.value = nhour.value;
+                listItem.appendChild(nhourInput)
+            }
+    
+            const listData1 = document.createElement('div');
+            listData1.classList.add('list-data');
+            const del = document.createElement('span');
+            del.classList.add('material-symbols-outlined', 'delete');
+            del.setAttribute('item-id', nextIndex);
+            del.innerText = 'delete';
+            del.addEventListener('click', function(){
+                removePlanDay(del);
+            })
+            listData1.appendChild(del);
+            const text1 = document.createElement('span');
+            text1.innerText = `${translateDaysOfWeek(dow.value)} ${hour.value}`;
+            listData1.appendChild(text1);
+            listItem.appendChild(listData1);
+    
+            if(check.checked){
+                const listData2 = document.createElement('div');
+                listData2.classList.add('list-data');
+                const noti = document.createElement('span');
+                noti.classList.add('material-symbols-outlined');
+                noti.innerText = 'notifications';
+                listData2.appendChild(noti);
+                const text2 = document.createElement('span');
+                text2.innerText = `${translateDaysOfWeek(ndow.value)} ${nhour.value}`;
+                listData2.appendChild(text2);
+                listItem.appendChild(listData2);
+            }
+            container.appendChild(listItem);
+            
+            nextIndex++;
+            document.getElementById('training_day_creator').classList.add('hidden');
+            dow.value = "";
+            hour.value = "";
+            check.checked = false;
+            ndow.value = "";
+            nhour.value = "";
+            ndow.disabled = true;
+            nhour.disabled = true;
+            document.getElementById('day-error').innerText = "";
+        }else{
+            document.getElementById('day-error').innerText = "Dodałeś już wcześniej dzień treningowy o takim samym dniu i godzinie.";
         }
 
-        const listData1 = document.createElement('div');
-        listData1.classList.add('list-data');
-        const del = document.createElement('span');
-        del.classList.add('material-symbols-outlined', 'delete');
-        del.setAttribute('item-id', nextIndex);
-        del.innerText = 'delete';
-        del.addEventListener('click', function(){
-            removePlanDay(del);
-        })
-        listData1.appendChild(del);
-        const text1 = document.createElement('span');
-        text1.innerText = `${translateDaysOfWeek(dow.value)} ${hour.value}`;
-        listData1.appendChild(text1);
-        listItem.appendChild(listData1);
-
-        if(check.checked){
-            const listData2 = document.createElement('div');
-            listData2.classList.add('list-data');
-            const noti = document.createElement('span');
-            noti.classList.add('material-symbols-outlined');
-            noti.innerText = 'notifications';
-            listData2.appendChild(noti);
-            const text2 = document.createElement('span');
-            text2.innerText = `${translateDaysOfWeek(ndow.value)} ${nhour.value}`;
-            listData2.appendChild(text2);
-            listItem.appendChild(listData2);
-        }
-        container.appendChild(listItem);
-        
-        nextIndex++;
-        document.getElementById('training_day_creator').classList.add('hidden');
-        dow.value = "";
-        hour.value = "";
-        check.checked = false;
-        ndow.value = "";
-        nhour.value = "";
     });
 
     document.querySelectorAll('.delete').forEach(function(del){
