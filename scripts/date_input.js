da54c395aae04f44c2e9c5701da3dcd41bdded59
maciejbilinski <80822798+maciejbilinski@users.noreply.github.com>
2023-01-21 21:57:15 +0100

    Training plan creator backend & frontend fixes (#10)

--- scripts/date_input.js
+++ scripts/date_input.js
@@ -1,5 +1,21 @@
+function formatDate(d) {
+    var month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return [year, month, day].join('-');
+}
 window.addEventListener('load', function(){
+    var date = new Date();
+    date.setDate(date.getDate() + 1)
+
     document.querySelectorAll('input.date-input').forEach(function(input){
+        input.min = formatDate(date)
         input.addEventListener('focus', function(){
             input.type = 'date';
         });
